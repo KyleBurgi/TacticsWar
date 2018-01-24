56b29280466d9175cf29751f0ae69689f2355844
Kyle Burgi <KyleBurgi@gmail.com>
2018-01-24 15:58:17 -0800

    Hello World, you are born.

---
+++ TacticsWar/TacticsWar/TacticsWarMain.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace TacticsWar
+{
+    class TacticsWarMain
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello World! This project is born.");
+            ReadLine();
+        }
+    }
+}
